31107e6ecc4a2d418709da068ad3eaa5e8af0264
solaris2006 <cristian.stefan@gmail.com>
2022-06-15 11:41:29 +0300

    modified part1/part1 App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -2,47 +2,79 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
 
-
-
-
-
 const App = () => {
-const [notes, setNotes] = useState([])
-const [newNote, setNewNote] = useState('')
-const [showAll, setShowAll] = useState(true)
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
+  const get = () => {
+    axios.get("http://localhost:3001/notes").then((response) => {
+      setNotes(response.data);
+    });
+  };
 
-const hook = () => {
-  axios
-    .get('http://localhost:3001/notes')
-    .then(response => {
-      setNotes(response.data)
-    })
-}
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() < 0.5,
+    };
 
-// useEffect(() => {
-//   // console.log('effect');
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
+  };
 
-//   axios
-//     .get('http://localhost:3001/notes')
-//     .then(response => {
-//       // console.log('promise fulfilled');
-//       setNotes(response.data)
-//   })
-// }, [])
+  const handleChange = (event) => {
+    setNewNote(event.target.value);
+  };
 
-useEffect(hook, [])
-console.log('render', notes.length, 'notes');
+  const toggleImportance = (id) => {
+    console.log("importance of " + id + " needs to be toggled");
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
 
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });
+  };
+
+  // useEffect(() => {
+  //   // console.log('effect');
+
+  //   axios
+  //     .get('http://localhost:3001/notes')
+  //     .then(response => {
+  //       // console.log('promise fulfilled');
+  //       setNotes(response.data)
+  //   })
+  // }, [])
+
+  useEffect(get, []);
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportance(note.id)}
+          />
         ))}
       </ul>
+
+      <form onSubmit={addNote}>
+        add note <input onChange={handleChange} value={newNote}></input>
+        <div>
+          {" "}
+          <button type="submit"> Add</button>
+        </div>
+      </form>
     </div>
   );
 };
